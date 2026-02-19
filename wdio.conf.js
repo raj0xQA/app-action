821cb228888852cde46bc37be7f7ec5403fa2ead
raj <raj@premio.io>
2026-02-19 11:29:40 +0600

    abc

--- wdio.conf.js
+++ wdio.conf.js
@@ -48,11 +48,11 @@ export const config = {
     // "appium:platformVersion": "14",
     "appium:automationName": "UiAutomator2",
     "appium:app": process.env.LOCAL_APP,
-    // "appium:autoGrantPermissions": true,
-    // "appium:skipDeviceInitialization": true,
-    // "appium:skipServerInstallation": true,
-    // "appium:fullReset": false,
-    // "appium:noReset": true,
+    "appium:autoGrantPermissions": true,
+    "appium:skipDeviceInitialization": true,
+    "appium:skipServerInstallation": true,
+    "appium:fullReset": false,
+    "appium:noReset": true,
     // 'goog:chromeOptions': {
     //   args: [
     //     '--user-agent=Mozilla/5.0 (Linux; Android 13; SM-S908U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.144 Mobile Safari/537.36'
